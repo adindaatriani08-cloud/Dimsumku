5cfe6a34a1f8e1d2d2d5925d377ba14a48fafc0d
adindaatriani08-cloud <adindaatriani08@gmail.com>
2026-03-25 21:14:24 +0700

    bismillah terakhir

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-       <BrowserRouter basename="Dimsumku/">
+       <BrowserRouter basename="/Dimsumku/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
